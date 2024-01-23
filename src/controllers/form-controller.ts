e04658f272b38076f99df80723c115fcc375a182
Giorgi Kantsliani <gqancliani@gmail.com>
2024-01-23 21:46:17 +0400

    feat: created request to submit the form

--- src/controllers/form-controller.ts
+++ src/controllers/form-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import Form from "models/Form.js";
+import addFormSchema from "schemas/addFormSchema.js";
 
 
 export const createForm = async (req: Request, res: Response) => {
@@ -6,6 +8,33 @@ export const createForm = async (req: Request, res: Response) => {
   const { body } = req;
 
   try {
+
+    const validator = await addFormSchema(body);
+    
+    const { value, error } = validator.validate(body);
+
+    if (error) {
+      return res.status(400).json(error);
+    }
+
+    const { name, email, phoneNumber, plan, billing, addons} = value;
+
+    const form = await Form.findOne({ email });
+
+    if (form) {
+      return res.status(400).json({ message: "Form has been already submitted with this email" });
+    }
+
+    const newForm = new Form({
+      name,
+      email,
+      phoneNumber,
+      plan,
+      billing,
+      addons,
+    });
+
+    await newForm.save();
     
   } catch (error) {
     return res.status(401).json(error)
