410b12904bfd669f44fa35859da4fead02cdb01a
Giorgi Kantsliani <gqancliani@gmail.com>
2024-01-23 20:32:45 +0400

    chore: added swagger middlware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import BodyParser from 'body-parser';
 import cors from 'cors';
+import { swaggerMiddleware } from './middlewares';
 
 const server = express();
 server.use(cors());
 server.use(BodyParser.json());
 
-server.listen(3000);
+server.use("/", ...swaggerMiddleware)
+
+server.listen(process.env.PORT || 3000);
